8848648fbe42bfaefe57d9907adace39430a2f91
raj7desai <raj7desai@gmail.com>
2016-11-13 04:56:19 +0530

    Trying to figure out the undefined google error

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function initialize() {
 	service.nearbySearch(request, callback);
 }
 
-function initMap(results, status) {
+function callback(results, status) {
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
