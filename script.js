3e01979a199ab218ede2fd88855e106ed795b2fe
raj7desai <raj7desai@gmail.com>
2016-11-13 03:57:42 +0530

    Map loads. Edits to script.js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 var map;
-var service;
 var infowindow;
 
-function initMap() {
-  var pyrmont = new google.maps.LatLng(19.063741, 72.831356);
+function initialize() {
+	var pyrmont = new google.maps.LatLng(11.2333, 78.1667);
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 19.063741, lng: 72.831356},
-		zoom: 13
-    });
+	map = new google.maps.Map(document.getElementById('map-canvas'), {
+		center: pyrmont,
+		zoom: 15
+	});
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    query: 'restaurant'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+	var request = {
+		location: pyrmont,
+		radius: 5500,
+		types: ['atm']
+	};
+	infowindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(request, callback);
 }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
+function initMap(results, status) {
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		for (var i = 0; i < results.length; i++) {
+			createMarker(results[i]);
+		}
+	}
 }
+
+function createMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({map: map, position: place.geometry.location});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent(place.name);
+		infowindow.open(map, this);
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
